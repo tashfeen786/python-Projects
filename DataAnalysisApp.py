84fb3fd8a0352ba09ffeaa6d7ee56e4cf8e44b00
tashfeen786 <tashfeenakhonzada786@gmail.com>
2025-07-23 00:39:00 +0500

    "Added a subheader to the Data Analysis App."

--- DataAnalysisApp.py
+++ DataAnalysisApp.py
@@ -6,3 +6,5 @@ import seaborn as sns
 
 # Set the title of the app
 st.title("Data Analysis App")
+st.subheader("This is a Simple data analysis application built with Streamlit and Python Created by Tashfeen Aziz")
+
